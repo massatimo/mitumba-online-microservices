19a55b9f13b0da3f4524af2b81f58a0cda56398f
Masai Timothy <massatimo@gmail.com>
2026-03-29 14:27:32 +0300

    Done(week2): REST API specification, MongoDB schemas, service communication plan, and full development environment with cloud database integration MongoDB

---
+++ order-services/src/server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected for User Service'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
+app.get('/health', (req, res) => {
+  res.json({ status: '✅ User Service is running', port: process.env.PORT_USER, db: 'connected' });
+});
+
+const PORT = process.env.PORT_USER || 3003;
+app.listen(PORT, () => {
+  console.log('🚀 User Service running on http://localhost:' + PORT);
+});
---
+++ product-services/src/models/Product.js
@@ -0,0 +1,11 @@
+const productSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  size: { type: String, enum: ['S','M','L','XL','XXL'] },
+  brand: String,
+  condition: { type: String, enum: ['New', 'Gently Used', 'Used', 'Fagi'] },
+  images: [String],
+  vendorId: { type: Schema.Types.ObjectId, ref: 'User' },
+  stock: { type: Number, default: 1 }
+});
