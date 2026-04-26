4ab49537544354c51617078116b73bfaac6e8cd7
Masai Timothy <massatimo@gmail.com>
2026-04-26 06:26:15 +0300

    feat(week5): Complete Order Service microservice + inter-service foundation Developed Order Service with Order model and basic CRUD - Implemented placeOrder and getMyOrders endpoints - Added JWT authentication middleware - Prepared for inter-service communication (Order ↔ Product) - All three microservices now running and connected to MongoDB Atlas

---
+++ order-services/src/controllers/orderController.js
@@ -0,0 +1,32 @@
+﻿const Order = require('../models/Order');
+const axios = require('axios');   // for calling other services
+
+// Place Order
+exports.placeOrder = async (req, res) => {
+  try {
+    const { items } = req.body;
+    // In Week 5 we will add real communication with Product Service
+    const totalAmount = items.reduce((sum, item) => sum + item.priceAtPurchase * item.quantity, 0);
+
+    const order = new Order({
+      buyerId: req.user.id,
+      items,
+      totalAmount,
+      moMoReference: 'MOMO-' + Date.now()
+    });
+
+    await order.save();
+    res.status(201).json({ success: true, message: 'Order placed successfully', data: order });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+exports.getMyOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ buyerId: req.user.id });
+    res.json({ success: true, data: orders });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
